85b5b09682f5153bd2562373bcfa076193140d07
l34r00t <leandro@leandropintos.com>
2020-09-19 05:12:08 -0300

    Update server.tf

--- digital-ocean/server.tf
+++ digital-ocean/server.tf
@@ -8,7 +8,7 @@ resource "digitalocean_droplet" "vm_instance" {
   count       = var.vps_number
   image       = "ubuntu-18-04-x64"
   name        = "mainRecon-vps-${random_id.instance_id[count.index].hex}"
-  region      = var.region 
+  region      = var.region
   size        = "s-2vcpu-4gb"
   vpc_uuid    = digitalocean_vpc.vpc_network.id
   ssh_keys    = [digitalocean_ssh_key.ssh_public_key.fingerprint]
@@ -25,5 +25,37 @@ data "template_file" "cloud_init_yaml" {
   vars = {
     ssh_public_key = file(var.ssh_public_key)
     username = var.username
+  }
+}
+
+data "template_file" "dev_hosts" {
+  template = "${file("${path.module}/templates/host.cfg")}"
+  depends_on = [
+    digitalocean_droplet.vm_instance,
+  ]
+  vars= {
+    externalIP = join("\n", digitalocean_droplet.vm_instance.*.ipv4_address)
+  }
+}
+
+resource "null_resource" "sleep-before-ansible" {
+  triggers = {
+    template_rendered = data.template_file.dev_hosts.rendered
+  }
+
+  provisioner "local-exec" {
+    command = "sleep 60"
+  }
+}
+
+resource "null_resource" "dev-hosts" {
+  depends_on = [
+    null_resource.sleep-before-ansible,
+  ]
+  provisioner "local-exec" {
+    command = "echo '${data.template_file.dev_hosts.rendered}' > ../ansible/hosts.yaml"
+  }
+  provisioner "local-exec" {
+    command = "cd ../ansible  && ansible-playbook playbook.yaml "
   }
 }
